6277d2d70530f45add905386c1dc19ea86da2640
Ozan Taşar <tasarozan@gmail.com>
2021-06-19 22:26:42 +0300

    Rearrange files

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
   #app
-    h1 caravansary
     #nav
       router-link(to="/") Home |&nbsp;
       router-link(to="/about") About
@@ -12,7 +11,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ frontend/src/views/home.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+    this.users = usersRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+      .home
+        h1 CARAVANSARY
+        user-card(v-for="user in users" :user="user")
+</template>
