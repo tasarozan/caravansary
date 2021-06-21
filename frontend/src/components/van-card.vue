863fab4148fe1f2e648e582da3bed765183df9d4
Ozan Taşar <tasarozan@gmail.com>
2021-06-21 23:54:35 +0300

    Add van card component

---
+++ frontend/src/components/van-card.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'VanCard',
+  props: ['van'],
+}
+</script>
+
+<template lang="pug">
+.box
+  h2 Type: {{ van.type }}
+  h2 Location: {{ van.location }}
+  h2 Price: {{ van.price }}
+  h2 Owner: {{ van.owner.firstName }} {{ van.owner.lastName }}
+
+</template>
+
+<style lang="scss">
+.box {
+  padding: 2rem;
+  border: 1px solid #3339ff;
+  background: #8791f3;
+  border-radius: 0.3rem;
+}
+</style>
