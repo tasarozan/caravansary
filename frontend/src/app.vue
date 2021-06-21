1754d566496011fbbc4d62c0d69c39a13c6f1eac
Ozan Taşar <tasarozan@gmail.com>
2021-06-21 23:53:16 +0300

    Change router links from home and about to user list and vans

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") Home |&nbsp;
-      router-link(to="/about") About
+      router-link(to="/") User List |&nbsp;
+      router-link(to="/vans") Vans
+
     router-view
 </template>
 
