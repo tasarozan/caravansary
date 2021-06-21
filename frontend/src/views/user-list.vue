5c938adc69824b9ca2bce130031ff1cfb647d4cc
Ozan Taşar <tasarozan@gmail.com>
2021-06-21 23:50:47 +0300

    Add vans, user-detail and user-list files

---
+++ frontend/src/views/user-list.vue
@@ -0,0 +1,25 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'UserList',
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 CARAVANSARY
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.firstName }} {{ user.lastName }}
+</template>
---
+++ frontend/src/views/vans.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios'
+import VanCard from '@/components/van-card.vue'
+
+export default {
+  name: 'About',
+  components: {
+    VanCard,
+  },
+  data() {
+    return {
+      vans: [],
+    }
+  },
+  async created() {
+    const vansRequest = await axios.get('/api/vans')
+    console.log(vansRequest)
+    this.vans = vansRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .vans
+    van-card(v-for="van in vans" :van="van")
+    .buttons
+      button Share your Van
+</template>
+
+<style lang="scss">
+.buttons {
+  text-align: center;
+  background-color: blue;
+}
+</style>
