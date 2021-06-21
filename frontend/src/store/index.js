f71043a395edc6e797a7f2dca66853033622aad7
Ozan Taşar <tasarozan@gmail.com>
2021-06-21 23:55:12 +0300

    Implement fetchUser and fetchUsers functions to actions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async fetchUser(store, id) {
+      const userRequest = await axios.get(`/api/users/${id}`)
+      return userRequest.data
+    },
+    async fetchUsers() {
+      const usersRequest = await axios.get('/api/users')
+      return usersRequest.data
+    },
+  },
   modules: {},
 })
