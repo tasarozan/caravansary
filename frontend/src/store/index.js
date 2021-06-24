56e9cf1431fa6cdce2dbc0a3e315b42849e62d78
Ozan Taşar <tasarozan@gmail.com>
2021-06-24 18:08:46 +0300

    Add backend communication for authentication and van creation

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,9 +4,24 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+const mutations = {
+  SET_USER: 'set user',
+  CREATE_VAN: 'create van',
+}
+
+const store = new Vuex.Store({
+  state: {
+    user: null,
+    van: null,
+  },
+  mutations: {
+    [mutations.SET_USER](state, user) {
+      state.user = user
+    },
+    [mutations.CREATE_VAN](state, van) {
+      state.van = van
+    },
+  },
   actions: {
     async fetchUser(store, id) {
       const userRequest = await axios.get(`/api/users/${id}`)
@@ -20,6 +35,30 @@ export default new Vuex.Store({
       const vansRequest = await axios.get('/api/vans')
       return vansRequest.data
     },
+    async fetchSession({ commit }) {
+      const user = await axios.get('/api/account/session')
+      commit(mutations.SET_USER, user.data || null)
+    },
+    async login({ commit }, credentials) {
+      const user = await axios.post('/api/account/session', credentials)
+      commit(mutations.SET_USER, user.data)
+    },
+    async register(store, user) {
+      return axios.post('/api/account', user)
+    },
+    async logout({ commit }) {
+      await axios.delete('/api/account/session')
+      commit(mutations.SET_USER, null)
+    },
+    async createVan({ commit }, credentials) {
+      const van = await axios.post('/api/vans', credentials)
+      commit(mutations.CREATE_VAN, van.data)
+    },
   },
   modules: {},
 })
+
+export default async function init() {
+  await store.dispatch('fetchSession')
+  return store
+}
