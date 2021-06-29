193fa0796726f9a9547af91dd7147929ea8ac170
Ozan Ta?ar <tasarozan@gmail.com>
2021-06-29 13:54:13 +0300

    Send the error page instead of render

--- backend/src/app.js
+++ backend/src/app.js
@@ -78,14 +78,18 @@ app.use((req, res, next) => {
 })
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.send({
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') == 'development' ? err.stack : '',
+  })
 })
 
 module.exports = app
