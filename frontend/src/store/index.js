75961616e6c42a0d7c6220788a93e5e18b0c8e26
Ozan Taşar <tasarozan@gmail.com>
2021-07-05 17:41:55 +0300

    Implement backend url for deployment

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
+axios.defaults.withCredentials = true
+
 Vue.use(Vuex)
 
 const mutations = {
